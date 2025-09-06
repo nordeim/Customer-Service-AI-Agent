7e965f8f8fa8e735bb551974011794dbdffea73e
heinazhik <heinazhikweymong@outlook.com>
2025-09-06 22:45:45 +0800

    docker fix

--- pip-install.sh
+++ pip-install.sh
@@ -7,3 +7,6 @@ python -c 'import secrets; print(secrets.token_hex(32))'
 poetry run python scripts/test_business_logic.py
 
 poetry run python scripts/test_ai_services.py
+
+# docker-compose up -d postgres
+# docker exec -it ai-customer-service-postgres psql -U ai_agent -d ai_customer_service -c "SELECT extname FROM pg_extension WHERE extname = 'vector';"
